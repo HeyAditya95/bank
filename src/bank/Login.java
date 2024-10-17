d0a8e2014a953ecdd26834dd9a9cfba0c433a06f
Aditya jha <haaditya95b@gmail.com>
2024-10-17 19:46:26 +0530

    first commit on bank repo

---
+++ src/bank/Login.java
@@ -0,0 +1,16 @@
+package bank;
+
+import javax.swing.*;
+
+public class Login extends JFrame {
+    Login(){
+        setSize(800 , 400);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new Login();
+
+        }
+    }
+
